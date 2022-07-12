f379e28c9cf543ab1404a9a873f85b2e18a68b63
Amaury Soto <amaurysotog@gmail.com>
2022-07-12 16:16:44 -0500

    scope: variables  scope

---
+++ src/scope/re.js
@@ -0,0 +1,23 @@
+var firtsName; //undefined
+firtsName = 'Amaury'
+console.log(firtsName)
+
+
+var lastName = 'Soto' //Soto
+var lastName = 'Espitia'//Espitia
+lastName = 'Garay' //Garay
+console.log(lastName) 
+
+//let 
+let fruitLet = 'Apple'
+//xxxxx let fruitLet = 'Bananon' xxxx
+fruitLet = 'Banana'
+console.log(fruitLet)
+
+
+//const 
+
+const animal = 'Dog'
+animal = 'Cat'
+//const animal = 'Cat'
+ console.log(animal)
