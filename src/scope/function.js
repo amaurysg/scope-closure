f455b07bd64a390993c2181cce8f69ccbef16408
Amaury Soto <amaurysotog@gmail.com>
2022-07-12 14:30:24 -0500

    scope: function scope

---
+++ src/scope/function.js
@@ -0,0 +1,11 @@
+function greeting(){
+    let username = 'Amaury' 
+    //console.log(username)
+    if(username === 'Amaury'){
+        console.log(`Hola ${username}`)
+    }else{
+        console.log(`No te identifico`)
+    }
+}
+console.log(username,'out')
+greeting()
